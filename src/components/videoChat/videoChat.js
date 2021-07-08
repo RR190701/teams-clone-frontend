efc8a219cf096ab5eca41b30d15773ffd36823c6
Rashika <rashikarawat01@gmail.com>
2021-07-08 16:23:49 +0530

    added chat rome feature for before, after and in meeting

--- src/components/videoChat/videoChat.js
+++ src/components/videoChat/videoChat.js
@@ -11,10 +11,8 @@ import Chat from './../Chat/chat';
 import Snackbar from '@material-ui/core/Snackbar';
 import Slide from '@material-ui/core/Slide';
 import Lottie from 'react-lottie';
-import FullscreenIcon from '@material-ui/icons/Fullscreen';
 import * as heart from './../../utils/heart-lottie.json'
 import * as celebrate from "./../../utils/claps.json";
-import Tooltip from '@material-ui/core/Tooltip';
 
 function SlideTransition(props) {
   return <Slide {...props} direction="up" />;
@@ -67,14 +65,7 @@ const VideoChat = (props) => {
       });
 
     // Set Back Button Event
-    window.addEventListener('popstate', goToBack); 
-
-    //tab switch events
-  //   window.addEventListener('beforeunload', function (e) {
-  //     e.preventDefault();
-  //     e.returnValue = '';
-  // });
-    
+    window.addEventListener('popstate', goToBack);     
 
    // Connect Camera & Mic
     navigator.mediaDevices
@@ -427,7 +418,6 @@ function createUserVideo(peer, index, arr) {
     const goToBack = (e) => {
       e.preventDefault();
       socket.emit('B-leave-room', { roomID, leaver: currentUser });
-      sessionStorage.removeItem('username');
       window.location.href = '/';
     };
   
